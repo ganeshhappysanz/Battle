0a0ad2e11d84d1797cee92fa800d0475222b1e5b
ganeshhappysanz <ganesh@happysanz.com>
2020-12-29 08:24:09 -0800

    initial commit

---
+++ src/main/java/testpack/FTest.java
@@ -0,0 +1,40 @@
+package testpack;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class FTest {
+	
+WebDriver driver;
+	
+	@BeforeSuite
+	public void setup() {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Ganesh\\eclipse-workspace\\com.demotestmaven\\Drivers\\chromedriver.exe");
+		driver=new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.MILLISECONDS);
+	}
+	
+	@Test(priority = 1)
+	public void One() {
+		driver.get("https://google.com");
+	}
+	@Test(dependsOnMethods = "One")
+	public void two() {
+		driver.get("https://bing.com");
+	}
+	@Test(dependsOnMethods = "two")
+	public void three() {
+		driver.get("https://yahoo.com");
+	}
+	
+	@AfterSuite
+	public void teardown() {
+		driver.quit();
+	}
+
+}
